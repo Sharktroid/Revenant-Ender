4f9dbc5f92292b1881fbe8da7be473f75eeddc2f
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2023-04-27 11:58:15 -0400

    Removed unused files

--- test.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$Line2D.points = [Vector2i(0, 0), Vector2i(0, 16)]
-	$Line2D.points = [Vector2i(0, 16), Vector2i(16, 16)]
-	$Line2D.points = [Vector2i(16, 16), Vector2i(16, 0)]
-	$Line2D.points = [Vector2i(16, 0), Vector2i(0, 0)]
