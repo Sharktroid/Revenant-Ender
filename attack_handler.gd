2f78eca726999ec7a7696699088827f2f7d06301
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2023-05-08 19:40:45 -0400

    Update attack_handler.gd

--- attack_handler.gd
+++ attack_handler.gd
@@ -9,9 +9,15 @@ static func combat(attacker: Unit, defender: Unit) -> void:
 static func map_combat(attacker: Unit, defender: Unit) -> void:
 	var attacker_animation: MapAttack = MapAttack.new(attacker, defender.position)
 	attacker.get_parent().add_child(attacker_animation)
+	var defender_animation: MapAttack = MapAttack.new(defender, attacker.position)
+	defender.get_parent().add_child(defender_animation)
 	attacker.visible = false
+	defender.visible = false
 	await _map_attack(attacker, defender, attacker_animation)
 	attacker.visible = true
+	attacker_animation.queue_free()
+	defender.visible = true
+	defender_animation.queue_free()
 
 
 static func _map_attack(attacker: FEUnit, defender: Unit, attacker_animation: MapAttack) -> void:
@@ -20,4 +26,3 @@ static func _map_attack(attacker: FEUnit, defender: Unit, attacker_animation: Ma
 	defender.add_current_health(-attacker.get_damage(defender))
 	attacker_animation.emit_signal("proceed")
 	await attacker_animation.complete
-	attacker_animation.queue_free()
