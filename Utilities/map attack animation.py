a6d7766b7d2f10bef8e0d3f0706839b82a9c9413
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2023-05-19 17:44:41 -0400

    Create map attack animation.py

---
+++ Utilities/map attack animation.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+def box_pos_size(x: int, y: int, width: int, height: int) -> tuple[int, int, int, int]:
+    return (x, y, x + width, y + height)
+
+cell_size = 64
+image_num = 9
+img_offset: tuple[int, int] = (256, 15)
+tile_offset: tuple[float, float] = (7, 7)
+all_images = []
+for image in range(1, image_num + 1):
+    with Image.open(f'Swordmaster-f-{image}.png', 'r') as anim:
+        box = box_pos_size(img_offset[0] + tile_offset[0] * 16 - (cell_size - 16)/2, img_offset[1] + tile_offset[1] * 16 - (cell_size - 16)/2, cell_size, cell_size)
+        all_images.append(anim.crop(box))
+
+full_image = Image.new('RGB', (cell_size * image_num, cell_size), (128, 160, 128))
+for i in range(image_num):
+    full_image.paste(all_images[i], ((i)*(cell_size), 0))
+full_image.save('Formatted.png')
