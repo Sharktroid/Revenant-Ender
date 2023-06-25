1263b9303f2768bfe0297ddf996a8dec35e6da55
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2023-06-24 20:37:48 -0400

    Game now starts

--- renderer.gd
+++ renderer.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 func _on_size_changed() -> void:
 	var scale_vector: Vector2 = DisplayServer.window_get_size()/GenVars.get_screen_size()
 	var scale: int = floor(min(scale_vector.x, scale_vector.y))
-	($SubViewportContainer.material as ShaderMaterial).set_shader_parameter("pixel_scale", scale)
+	(material as ShaderMaterial).set_shader_parameter("pixel_scale", scale)
