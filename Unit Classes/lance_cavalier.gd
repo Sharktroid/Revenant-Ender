9e7d3d4d6b32e0fa27134fde9ddac02d3c43a9d9
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2023-07-01 19:17:32 -0400

    Implemented base and end stats

--- Unit Classes/lance_cavalier.gd
+++ Unit Classes/lance_cavalier.gd
@@ -4,18 +4,50 @@ extends FEUnit
 # Unit-specific variables.
 func _init():
 	_unit_class = "Social Knight"
+	_max_level = 30
 	movement_type = movement_types.HEAVY_CAVALRY
 	_class_base_stats = {
-		stats.HITPOINTS: 20,
-		stats.STRENGTH: 5,
+		stats.HITPOINTS: 17,
+		stats.STRENGTH: 6,
 		stats.PIERCE: 0,
 		stats.MAGIC: 0,
-		stats.SKILL: 2,
+		stats.SKILL: 5,
 		stats.SPEED: 5,
-		stats.LUCK: 0,
-		stats.DEFENSE: 6,
-		stats.DURABILITY: 0,
-		stats.RESISTANCE: 0,
+		stats.LUCK: 3,
+		stats.DEFENSE: 5,
+		stats.DURABILITY: 4,
+		stats.RESISTANCE: 3,
 		stats.MOVEMENT: 7,
 		stats.CONSTITUTION: 9,
+		stats.LEADERSHIP: 0,
+	}
+	_class_end_stats = {
+		stats.HITPOINTS: 38,
+		stats.STRENGTH: 21,
+		stats.PIERCE: 0,
+		stats.MAGIC: 0,
+		stats.SKILL: 19,
+		stats.SPEED: 17,
+		stats.LUCK: 12,
+		stats.DEFENSE: 20,
+		stats.DURABILITY: 18,
+		stats.RESISTANCE: 17,
+		stats.MOVEMENT: 7,
+		stats.CONSTITUTION: 9,
+		stats.LEADERSHIP: 0,
+	}
+	_class_stat_caps = {
+		stats.HITPOINTS: 40,
+		stats.STRENGTH: 22,
+		stats.PIERCE: 0,
+		stats.MAGIC: 15,
+		stats.SKILL: 21,
+		stats.SPEED: 20,
+		stats.LUCK: 24,
+		stats.DEFENSE: 22,
+		stats.DURABILITY: 21,
+		stats.RESISTANCE: 21,
+		stats.MOVEMENT: 9,
+		stats.CONSTITUTION: 20,
+		stats.LEADERSHIP: 0,
 	}
--- Unit Classes/paladin.gd
+++ Unit Classes/paladin.gd
@@ -3,18 +3,50 @@ extends FEUnit
 
 func _init():
 	_unit_class = "Paladin"
+	_max_level = 50
 	movement_type = movement_types.ADVANCED_HEAVY_CAVALRY
 	_class_base_stats = {
-		stats.HITPOINTS: 23,
-		stats.STRENGTH: 7,
+		stats.HITPOINTS: 19,
+		stats.STRENGTH: 8,
 		stats.PIERCE: 0,
-		stats.MAGIC: 0,
-		stats.SKILL: 4,
+		stats.MAGIC: 1,
+		stats.SKILL: 7,
 		stats.SPEED: 7,
-		stats.LUCK: 0,
-		stats.DEFENSE: 8,
-		stats.DURABILITY: 0,
-		stats.RESISTANCE: 3,
+		stats.LUCK: 4,
+		stats.DEFENSE: 7,
+		stats.DURABILITY: 6,
+		stats.RESISTANCE: 6,
 		stats.MOVEMENT: 8,
-		stats.CONSTITUTION: 11,
+		stats.CONSTITUTION: 0,
+		stats.LEADERSHIP: 0,
+	}
+	_class_end_stats = {
+		stats.HITPOINTS: 51,
+		stats.STRENGTH: 28,
+		stats.PIERCE: 0,
+		stats.MAGIC: 1,
+		stats.SKILL: 25,
+		stats.SPEED: 25,
+		stats.LUCK: 16,
+		stats.DEFENSE: 29,
+		stats.DURABILITY: 27,
+		stats.RESISTANCE: 26,
+		stats.MOVEMENT: 8,
+		stats.CONSTITUTION: 0,
+		stats.LEADERSHIP: 0,
+	}
+	_class_stat_caps = {
+		stats.HITPOINTS: 60,
+		stats.STRENGTH: 31,
+		stats.PIERCE: 0,
+		stats.MAGIC: 26,
+		stats.SKILL: 30,
+		stats.SPEED: 30,
+		stats.LUCK: 32,
+		stats.DEFENSE: 32,
+		stats.DURABILITY: 32,
+		stats.RESISTANCE: 32,
+		stats.MOVEMENT: 10,
+		stats.CONSTITUTION: 0,
+		stats.LEADERSHIP: 0,
 	}
