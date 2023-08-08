7b6f955e7ab4a37800de82e38395079c3df1504a
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2023-08-08 13:39:27 -0400

    Can back out of every part of the menu

--- ui/status_menu/status_screen.gd
+++ ui/status_menu/status_screen.gd
@@ -3,6 +3,8 @@ extends Control
 
 func _ready() -> void:
 	grab_focus()
+	var internal_tab_bar: TabBar = ($"Unit Information Menu/Menu Tabs".get_child(0, true))
+	internal_tab_bar.mouse_filter = Control.MOUSE_FILTER_PASS
 
 
 func _gui_input(event: InputEvent) -> void:
