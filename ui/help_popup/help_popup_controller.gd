45f8f027c3b401e9f0a3fa7bbde1cf111035a5fd
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2023-09-13 11:37:14 -0400

    Update help_popup_controller.gd

--- ui/help_popup/help_popup_controller.gd
+++ ui/help_popup/help_popup_controller.gd
@@ -23,13 +23,11 @@ func shrink() -> void:
 	get_popup_node().custom_minimum_size = Vector2()
 	var min_size: Vector2 = get_popup_node().get_minimum_size()
 	var starting_ticks: int = Engine.get_physics_frames()
-	while true:
+	var get_weight: Callable = func(): return float(Engine.get_physics_frames() - starting_ticks)/SPEED
+	while get_weight.call() < 1:
 		await get_popup_node().get_tree().process_frame
-		var weight: float = float(Engine.get_physics_frames() - starting_ticks)/SPEED
-		if weight >= 1:
-			get_popup_node().custom_minimum_size = min_size
-			break
-		get_popup_node().custom_minimum_size = max_size.lerp(min_size, weight)
+		get_popup_node().custom_minimum_size = max_size.lerp(min_size, get_weight.call())
+	get_popup_node().custom_minimum_size = min_size
 	get_popup_node().visible = false
 	_active = false
 
@@ -39,7 +37,7 @@ func is_active() -> bool:
 
 
 func get_popup_node() -> Label:
-	return GenVars.map_controller.get_node("UI Layer/Help Popup")
+	return MapController.get_node("UI Layer/Help Popup")
 
 
 func _expand(text: String) -> void:
@@ -50,10 +48,8 @@ func _expand(text: String) -> void:
 	var max_size: Vector2 = get_popup_node().size
 	get_popup_node().text = ""
 	var starting_ticks: int = Engine.get_physics_frames()
-	while true:
+	var get_weight: Callable = func(): return float(Engine.get_physics_frames() - starting_ticks)/SPEED
+	while get_weight.call() < 1:
 		await get_popup_node().get_tree().process_frame
-		var weight: float = float(Engine.get_physics_frames() - starting_ticks)/SPEED
-		if weight >= 1:
-			get_popup_node().custom_minimum_size = max_size
-			break
-		get_popup_node().custom_minimum_size = min_size.lerp(max_size, weight)
+		get_popup_node().custom_minimum_size = min_size.lerp(max_size, get_weight.call())
+	get_popup_node().custom_minimum_size = max_size
