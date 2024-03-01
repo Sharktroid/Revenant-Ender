2cd4fa3ad9114886ac5fd70ff4c126c9632c862b
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-03-01 09:58:17 -0500

    Weapon ranks now support arrow keys

--- ui/map_ui/status_screen/item_screen/item_screen.gd
+++ ui/map_ui/status_screen/item_screen/item_screen.gd
@@ -5,6 +5,26 @@ const _ITEM_LABEL_NODE: PackedScene = \
 		preload("res://ui/map_ui/item_label/item_label.tscn")
 
 
+func _enter_tree() -> void:
+	var grid: GridContainer = $"Weapon Ranks/GridContainer"
+	var count: int = grid.get_child_count()
+	var subfunc: Callable = func(neighbor_name: String, index: int, modifier: int) -> void:
+		var new_index: int = index + modifier
+		if new_index >= 0 and new_index < count:
+			grid.get_child(index).set("focus_neighbor_%s" % neighbor_name,
+					grid.get_child(index).get_path_to(grid.get_child(new_index)))
+			if neighbor_name == "left":
+				print_debug(grid.get_child(index))
+				print_debug(grid.get_child(index).get("focus_neighbor_%s" % neighbor_name))
+	for index in count:
+		subfunc.call("top", index, -2)
+		subfunc.call("bottom", index, 2)
+		if (index % 2 == 0):
+			subfunc.call("right", index, 1)
+		else:
+			subfunc.call("left", index, -1)
+
+
 func update() -> void:
 	for child: Node in $"Item Panel/Item Label Container".get_children():
 		for grandchild: Node in child.get_children():
