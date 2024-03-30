ebf9baa0c4a89e542befe08d5e4757ea90dee1d3
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-03-30 10:27:51 -0400

    Fixed rounding error

--- ui/map_ui/status_screen/status_screen.gd
+++ ui/map_ui/status_screen/status_screen.gd
@@ -71,7 +71,7 @@ func _update() -> void:
 	var current_experience: float = observing_unit.get_current_experience()
 	var next_level_experience: float = Unit.get_experience_to_level(observing_unit.level + 1)
 	_set_label_text_to_number(%"Exp Percent" as Label,
-			floori(float(current_experience)/next_level_experience * 100))
+			floori(roundf(current_experience)/next_level_experience * 100))
 	(%"Exp Stat Help" as HelpContainer).help_description = \
 			"%d/%d\n%d to next level\nTotal experience: %d" % [
 				roundi(current_experience),
