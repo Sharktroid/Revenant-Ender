65e6ebca6aadaf631ea5aa62da80ab0c63e33dd7
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-03-30 10:27:41 -0400

    Added debug bow

---
+++ items/weapons/bows/debug_bow/debug_bow.gd
@@ -0,0 +1,18 @@
+class_name Debug_Bow
+extends Bow
+
+
+# Weapon-specific variables.
+func _init() -> void:
+	name = "Debug Bow"
+	level = ranks.E
+	might = 257
+	weight = 6
+	hit = 65537
+	crit = 0
+	max_uses = 40
+	price = 2200
+	weapon_experience = 1
+	_description = "A cheap, easy to wield bow."
+#	effective_classes
+	super()
