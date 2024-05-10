58dfbfb185bb26142a5b21a0b7d24582627ee45d
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-05-09 21:08:19 -0400

    Updated templates

--- script_templates/unit/unit_template.gd
+++ script_templates/unit/unit_template.gd
@@ -4,33 +4,28 @@ extends _BASE_
 
 # Unit-specific variables.
 func _init() -> void:
-_TS_personal_values = {
-_TS__TS_Unit.Stats.HIT_POINTS: 5,
-_TS__TS_Unit.Stats.STRENGTH: 5,
-_TS__TS_Unit.Stats.PIERCE: 5,
-_TS__TS_Unit.Stats.MAGIC: 5,
-_TS__TS_Unit.Stats.SKILL: 5,
-_TS__TS_Unit.Stats.SPEED: 5,
-_TS__TS_Unit.Stats.LUCK: 5,
-_TS__TS_Unit.Stats.DEFENSE: 5,
-_TS__TS_Unit.Stats.ARMOR: 5,
-_TS__TS_Unit.Stats.RESISTANCE: 5,
-_TS__TS_Unit.Stats.MOVEMENT: 5,
-_TS__TS_Unit.Stats.CONSTITUTION: 5,
-_TS__TS_Unit.Stats.LEADERSHIP: 5,
-_TS_}
-_TS_effort_values = {
-_TS__TS_Unit.Stats.HIT_POINTS: 0,
-_TS__TS_Unit.Stats.STRENGTH: 0,
-_TS__TS_Unit.Stats.PIERCE: 0,
-_TS__TS_Unit.Stats.MAGIC: 0,
-_TS__TS_Unit.Stats.SKILL: 0,
-_TS__TS_Unit.Stats.SPEED: 0,
-_TS__TS_Unit.Stats.LUCK: 0,
-_TS__TS_Unit.Stats.DEFENSE: 0,
-_TS__TS_Unit.Stats.ARMOR: 0,
-_TS__TS_Unit.Stats.RESISTANCE: 0,
-_TS__TS_Unit.Stats.MOVEMENT: 0,
-_TS__TS_Unit.Stats.CONSTITUTION: 0,
-_TS__TS_Unit.Stats.LEADERSHIP: 0,
-_TS_}
+_TS__personal_hit_points = 5
+_TS__personal_strength = 5
+_TS__personal_pierce = 5
+_TS__personal_intelligence = 5
+_TS__personal_skill = 5
+_TS__personal_speed = 5
+_TS__personal_luck = 5
+_TS__personal_defense = 5
+_TS__personal_armor = 5
+_TS__personal_resistance = 5
+_TS__personal_movement = 5
+_TS__personal_build = 5
+
+_TS_effort_hit_points =
+_TS_effort_strength =
+_TS_effort_pierce =
+_TS_effort_intelligence =
+_TS_effort_skill =
+_TS_effort_speed =
+_TS_effort_luck =
+_TS_effort_defense =
+_TS_effort_armor =
+_TS_effort_resistance =
+_TS_effort_movement =
+_TS_effort_build =
--- script_templates/unitclass/unit_class_template.gd
+++ script_templates/unitclass/unit_class_template.gd
@@ -9,35 +9,30 @@ _TS_name = ""
 _TS_movement_type = MovementTypes.
 _TS_base_weapon_levels
 _TS_max_weapon_levels
-_TS__class_base_stats = {
-_TS__TS_Unit.Stats.HIT_POINTS: 0,
-_TS__TS_Unit.Stats.STRENGTH: 0,
-_TS__TS_Unit.Stats.PIERCE: 0,
-_TS__TS_Unit.Stats.MAGIC: 0,
-_TS__TS_Unit.Stats.SKILL: 0,
-_TS__TS_Unit.Stats.SPEED: 0,
-_TS__TS_Unit.Stats.LUCK: 0,
-_TS__TS_Unit.Stats.DEFENSE: 0,
-_TS__TS_Unit.Stats.ARMOR: 0,
-_TS__TS_Unit.Stats.RESISTANCE: 0,
-_TS__TS_Unit.Stats.MOVEMENT: 0,
-_TS__TS_Unit.Stats.CONSTITUTION: 0,
-_TS__TS_Unit.Stats.LEADERSHIP: 0,
-_TS_}
-_TS__class_end_stats = {
-_TS__TS_Unit.Stats.HIT_POINTS: 0,
-_TS__TS_Unit.Stats.STRENGTH: 0,
-_TS__TS_Unit.Stats.PIERCE: 0,
-_TS__TS_Unit.Stats.MAGIC: 0,
-_TS__TS_Unit.Stats.SKILL: 0,
-_TS__TS_Unit.Stats.SPEED: 0,
-_TS__TS_Unit.Stats.LUCK: 0,
-_TS__TS_Unit.Stats.DEFENSE: 0,
-_TS__TS_Unit.Stats.ARMOR: 0,
-_TS__TS_Unit.Stats.RESISTANCE: 0,
-_TS__TS_Unit.Stats.MOVEMENT: 0,
-_TS__TS_Unit.Stats.CONSTITUTION: 0,
-_TS__TS_Unit.Stats.LEADERSHIP: 0,
-_TS_}
-_TS_map_sprite = preload("")
-_TS_default_portrait = preload("")
+
+_TS__base_hit_points =
+_TS__base_strength =
+_TS__base_pierce =
+_TS__base_intelligence =
+_TS__base_skill =
+_TS__base_speed =
+_TS__base_luck =
+_TS__base_defense =
+_TS__base_armor =
+_TS__base_resistance =
+_TS__base_movement =
+_TS__base_build =
+
+_TS__end_hit_points =
+_TS__end_strength =
+_TS__end_pierce =
+_TS__end_intelligence =
+_TS__end_skill =
+_TS__end_speed =
+_TS__end_luck =
+_TS__end_defense =
+_TS__end_armor =
+_TS__end_resistance =
+_TS__end_movement =
+_TS__end_build =
+_TS_super()
