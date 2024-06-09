59e48ed201d770e553512a1eaeb6c4e25e30022f
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-06-09 11:33:21 -0400

    Game pauses when unfocused

--- renderer/renderer.gd
+++ renderer/renderer.gd
@@ -8,6 +8,14 @@ func _enter_tree() -> void:
 	get_viewport().size_changed.connect(_on_size_changed)
 
 
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_APPLICATION_FOCUS_OUT:
+			get_tree().paused = true
+		NOTIFICATION_APPLICATION_FOCUS_IN:
+			get_tree().paused = false
+
+
 func _on_size_changed() -> void:
 	var scale_vector: Vector2 = DisplayServer.window_get_size() / Utilities.get_screen_size()
 	(material as ShaderMaterial).set_shader_parameter(
