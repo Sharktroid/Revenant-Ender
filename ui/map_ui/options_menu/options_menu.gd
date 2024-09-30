208886ada645147c0ede9cec7f6e0cb178bd900d
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-09-30 13:25:50 -0400

    Can now change selected setting

--- ui/map_ui/options_menu/options_menu.gd
+++ ui/map_ui/options_menu/options_menu.gd
@@ -7,6 +7,14 @@ var _options: Array[_OPTION] = [
 ]
 
 var _settings_indices: Array[int]
+var _current_setting_index: int:
+	get():
+		return _settings_indices[_current_index]
+	set(value):
+		_get_current_setting_label().theme_type_variation = &"GrayLabel"
+		_settings_indices[_current_index] = value % _options[_current_index].get_settings().size()
+		_get_current_setting_label().theme_type_variation = &"BlueLabel"
+		_update_column_hand_x()
 var _current_index: int = 0:
 	set(value):
 		_current_index = posmod(value, _option_count)
@@ -18,14 +26,17 @@ var _current_index: int = 0:
 			_top_index += 1
 		elif _get_relative_index() == 0:
 			_top_index -= 1
-		_hand_sprite.position.y = _hand_starting_y + _get_relative_index() * 16
+		_row_hand_sprite.position.y = _hand_starting_y + _get_relative_index() * 16
+		_column_hand_sprite.position.y = _hand_starting_y + _get_relative_index() * 16
+		_update_column_hand_x()
 var _top_index: int = 0
 var _displayed_item_count: int = _options.size()
 
 @onready var _scroll_container := %ScrollContainer as ScrollContainer
 @onready var _option_count: int = _options.size()
-@onready var _hand_sprite := $Hand as Sprite2D
-@onready var _hand_starting_y: int = roundi(_hand_sprite.position.y)
+@onready var _row_hand_sprite := $RowHand as Sprite2D
+@onready var _column_hand_sprite := $ColumnHand as Sprite2D
+@onready var _hand_starting_y: int = roundi(_row_hand_sprite.position.y)
 
 
 func _ready() -> void:
@@ -54,6 +65,7 @@ func _ready() -> void:
 			)
 			settings_h_box.add_child(setting_label)
 		%SettingsList.add_child(settings_h_box)
+	_update_column_hand_x.call_deferred()
 
 
 func _input(event: InputEvent) -> void:
@@ -61,6 +73,10 @@ func _input(event: InputEvent) -> void:
 		_current_index -= 1
 	if event.is_action_pressed("down", true):
 		_current_index += 1
+	if event.is_action_pressed("left", true):
+		_current_setting_index -= 1
+	if event.is_action_pressed("right", true):
+		_current_setting_index += 1
 
 
 func _process(_delta: float) -> void:
@@ -69,3 +85,13 @@ func _process(_delta: float) -> void:
 
 func _get_relative_index() -> int:
 	return _current_index - _top_index
+
+
+func _get_current_setting_label() -> Label:
+	return (%SettingsList).get_child(_current_index).get_child(_current_setting_index) as Label
+
+
+func _update_column_hand_x() -> void:
+	_column_hand_sprite.position.x = (
+		_get_current_setting_label().global_position.x - _column_hand_sprite.texture.get_size().x
+	)
